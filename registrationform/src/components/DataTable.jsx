f3cbff4141ffc2e92c4ecab6a70cfd749cc16792
gokuld <gokuld053@gmail.com>
2023-08-24 20:19:26 +0530

    registration form changes

--- registrationform/src/components/DataTable.jsx
+++ registrationform/src/components/DataTable.jsx
@@ -1,33 +1,188 @@
-import { Button, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+// import { Box, Button, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+
+import { DataGrid, GridActionsCellItem, GridRowModes} from '@mui/x-data-grid';
+import axios from "axios";
+import { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import { baseurl } from "../constants/AppConstants";
+import { Box } from '@mui/material';
+import moment from 'moment/moment';
+import { Cancel, Delete, Edit, Save } from '@mui/icons-material';
 
 const DataTable = () => {
-  const tableHeadings = ["Id","Name", "Email", "Phone Number", "Message", "Updated At", "Created At"];
-  const navigate = useNavigate();
-    return (
-        <>
-          <Container>
-            <TableContainer variant="outlined" component={Paper}>
-            <Table size="small" aria-label="simple table">
-              <TableHead>
-                <TableRow >
-                {
-                  tableHeadings.map(heading => {
-                    return (
-                      <TableCell align="center" sx={{textSizeAdjust:"auto"}}>{heading}</TableCell>
-                    )
-                  })}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                    <></>
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <Button variant="contained" onClick={() => navigate('/registration')}>Return</Button>
-          </Container>
-        </>
-    );
+    const[rowModesModel, setRowModesModel] = useState([]);
+    const handleEditClick = (id) => () => {
+      setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+    };
+    const handleSaveClick = (id) => () => {
+      setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    };
+    const handleDeleteClick = (id) => () => {
+      
+    };
+    const handleCancelClick = (id) => () => {
+      setRowModesModel({
+        ...rowModesModel,
+        [id]: { mode: GridRowModes.View, ignoreModifications: true },
+      });
+    };
+    const handleRowModesModelChange = (newRowModesModel) => {
+      setRowModesModel(newRowModesModel);
+    };
+    const [paginationModel, setPaginationModel] = useState({
+      pageSize: 5,
+      page: 0,
+    });
+    const tableHeadings = [
+    { 
+      field: 'id',
+      headerName: 'S.No',
+      type: 'number',
+      width: 20
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      editable: true,
+    }, 
+    {
+      field: 'email',
+      headerName: 'E-Mail',
+      editable: true,
+    },
+    {
+      field: 'phone_number',
+      headerName: 'Phone Number',
+      editable: true,
+    },
+    {
+      field: 'message',
+      headerName: 'Message',
+      editable: true,
+    },
+    {
+      field: 'updatedAt',
+      headerName: 'Updated At',
+      type: 'date',
+      valueFormatter: params => 
+        moment(params?.value).format("DD/MM/YYYY hh:mm A"),
+    },
+    {
+      field: 'createdAt',
+      headerName: 'Created At',
+      type: 'date',
+      valueFormatter: params => 
+        moment(params?.value).format("DD/MM/YYYY hh:mm A")
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: 'Actions',
+      width: 100,
+      cellClassName: 'actions',
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+        if(isInEditMode) {
+          return[
+            <GridActionsCellItem
+              icon={<Save />}
+              label="Save"
+              onClick={handleSaveClick(id)}
+              sx={{
+                color: 'primary.main',
+              }}
+            />,
+            <GridActionsCellItem
+              icon={<Cancel />}
+              label="Cancel"
+              onClick={handleCancelClick(id)}
+              className="textPrimary"
+              sx={{
+                color: 'primary.main',
+              }}
+            />
+            ];
+        }
+        return [
+          <GridActionsCellItem
+            icon={<Edit />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            sx={{
+              color: 'primary.main',
+            }}
+          />,
+          <GridActionsCellItem
+            icon={<Delete />}
+            label="Delete"
+            onClick={handleDeleteClick}
+            sx={{
+              color: 'primary.main',
+            }}
+          />,
+        ];
+      }
+    }
+  ];
+  const [tableData, setTableData] = useState([]); 
+  useEffect(()=>{
+    axios.get(baseurl + "admin/testing/getallusers?page=3&size=5")
+    .then((response => {
+      console.log(response);
+      setTableData(response?.data?.response?.paginationOutput?.results)
+    }))
+  },[]);
+  return (
+    <>
+    <Box sx={{height: 400, width: '100%' }}>
+      <DataGrid
+        disableRowSelectionOnClick
+        rows={tableData}
+        columns={tableHeadings}
+        paginationModel = {paginationModel}
+        onPaginationModelChange={setPaginationModel}
+        pageSizeOptions={[5]}
+        onRowModesModelChange={handleRowModesModelChange}
+      />
+    </Box>
+    </>
+      // <>
+      //   <Container>
+      //     <TableContainer variant="elevation" component={Paper}>
+      //     <Table size="small" aria-label="simple table">
+      //       <TableHead>
+      //         <TableRow >
+      //         {
+      //           tableHeadings.map(heading => {
+      //             return (
+      //               <TableCell align="center" sx={{textSizeAdjust:"auto"}}>{heading}</TableCell>
+      //             )
+      //           })}
+      //         </TableRow>
+      //       </TableHead>
+      //       <TableBody>
+      //       {tableData?.map((data) => {
+      //             return (
+      //               <TableRow>
+      //                 <TableCell align="center">{data.id}</TableCell>
+      //                 <TableCell align="center">  {data.name} </TableCell>
+      //                 <TableCell align="center"> {data.email} </TableCell>
+      //                 <TableCell align="center">{data.phone_number}</TableCell>
+      //                 <TableCell align="center">{data.message}</TableCell>
+      //                 <TableCell align="center">{data.createdAt}</TableCell>
+      //                 <TableCell align="center">{data.updatedAt}</TableCell>
+      //                 <TableCell align="center"><Button>Edit</Button></TableCell>
+      //               </ TableRow>
+      //             )
+      //           })}
+      //       </TableBody>
+      //     </Table>
+      //   </TableContainer>
+      //   <Button variant="contained" onClick={() => navigate('/registration')}>Return</Button>
+      //   </Container>
+      // </>
+  );
 }
 
 export default DataTable;
--- registrationform/src/components/UserInput.jsx
+++ registrationform/src/components/UserInput.jsx
@@ -1,9 +1,27 @@
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Box, Button, Grid, Modal, TextField, Typography } from "@mui/material";
+import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { baseurl } from "../constants/AppConstants";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '60%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 const UserInput = () => {
     const navigate = useNavigate();
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     const [user, setUser] = useState({
         name:"",
         email:"",
@@ -11,11 +29,44 @@ const UserInput = () => {
         message:""
     });
     const handleUserInput = (event) => {
-        console.log(event);
         setUser({ ...user,
             [event.target.name]: event.target.value
         });
     } 
+    const handleSubmit = (event) => {
+        handleOpen();
+    }
+    const handlePostData = (event) => {
+        axios.post(baseurl + "user/newRegistration", user)
+        .then((response)=> {
+            console.log(response);
+            if(response.status === 200) {
+                toast.success(response?.data?.response?.message, {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: false,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                    });
+                handleClose();
+            } 
+        }).catch((e)=>{
+            toast.error(e?.response?.data?.message, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                });
+            handleClose();
+        })
+    }
     return(
         <>
             <Grid container minHeight="500px" textAlign="center" alignItems="center">
@@ -26,8 +77,19 @@ const UserInput = () => {
                     <TextField fullWidth sx={{mb:2}} size="small" onChange={handleUserInput} variant="outlined" name="email" label="Email" />
                     <TextField fullWidth sx={{mb:2}} size="small" onChange={handleUserInput} variant="outlined" name="phone_number" label="Phone Number" />
                     <TextField fullWidth sx={{mb:2}} size="small" onChange={handleUserInput} variant="outlined" name="message" label="Message" />
-                    <Button variant="contained" sx={{mx:1}} onClick={() => navigate('/data')}>Submit</Button>
+                    <Button variant="contained" sx={{mx:1}} onClick={handleSubmit}>Submit</Button>
                     <Button variant="contained" sx={{mx:1}} onClick={() => navigate('/data')}>Show Table</Button>
+                    <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+                        <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Are you sure want to Submit?
+                        </Typography>
+                        <Box textAlign="end" marginTop="30px">
+                            <Button onClick={handleClose}>Cancel</Button><Button onClick={handlePostData}>Ok</Button>
+                        </Box>
+                        </Box>
+                    </Modal>
+                    <ToastContainer position="top-center" autoClose={5000} hideProgressBar newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover={false} theme="colored" />
                 </Grid>
                 <Grid items xs={3}></Grid>
             </Grid>
